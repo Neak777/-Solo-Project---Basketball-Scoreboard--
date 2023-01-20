cbcb6a6a7cadb6a24d364e22ae51f098b470e419
Neak777 <121606310+Neak777@users.noreply.github.com>
2023-01-21 00:28:09 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+ let scoRe = document.getElementById("score")
+let scoRe1 = document.getElementById("score1")
+ let count = 0
+ let count1 = 0
+ 
+ function increment (){
+     count +=  1
+     scoRe.textContent = count
+ }
+  
+  function increment2 (){
+     count = count + 2
+     scoRe.textContent = count
+ }
+ 
+   function increment3 (){
+     count = count + 3
+     scoRe.textContent = count
+ }
+ 
+  
+   function increment4 (){
+     count1 = count1 + 1
+     scoRe1.textContent = count1
+ }
+ 
+ 
+   function increment5 (){
+     count1 = count1 + 2
+     scoRe1.textContent = count1
+ }
+ 
+   function increment6 (){
+     count1 = count1 + 3
+     scoRe1.textContent = count1
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+ 
+ 
